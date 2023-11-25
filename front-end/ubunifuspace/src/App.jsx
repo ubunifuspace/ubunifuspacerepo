36d118149479028b346e1f890d947071a6f582b4
midnightCODA <mbunjimo@gmail.com>
2023-11-25 11:00:02 +0300

    added to react

--- front-end/ubunifuspace/src/App.jsx
+++ front-end/ubunifuspace/src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import Login from './Login'
-import SignUp from './SignUp'
+// import Login from './Login'
+// import SignUp from './SignUp'
+import { Homepage } from './pages'
+import './app.css'
 
 const App = () => {
   return (
     <div>
 {/* 
       <Login /> */}
+      
+      {/*
       <SignUp/>
+    */}
+
+    <Homepage />
 
     </div>
   )
